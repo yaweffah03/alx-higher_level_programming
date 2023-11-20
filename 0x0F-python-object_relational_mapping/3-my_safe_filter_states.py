18da9f7fe441f1b93d6370f699c4e53e7ffeeebc
yaweffah03 <mr.effah@icloud.com>
2023-11-19 17:20:20 -0800

    task

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
+"""script that takes in an argument and
+displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument and is safe from sql injections"""
+
+
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(
+            host="localhost", user=sys.argv[1],
+            passwd=sys.argv[2], db=sys.argv[3],
+            port=3306)
+
     cur = db.cursor()
-    match = sys.argv[4]
-    cur.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
+    cur.execute("SELECT * FROM states WHERE name = BINARY %s \
+        ORDER BY states.id ASC", (sys.argv[4],))
     rows = cur.fetchall()
     for row in rows:
         print(row)
